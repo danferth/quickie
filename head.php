cab3126af49afaf6c5a942f1653744e2f73797a3
danferth <danferth@gmail.com>
2014-08-21 17:12:35 -0700

    added font awsome

--- head.php
+++ head.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>just a quicky</title>
+	<title>just a quickie</title>
+	<link rel="stylesheet" type="text/css" href="css/font-awesome/font-awesome.css">
 	<link rel="stylesheet" type="text/css" href="css/css.css"> 
 </head>
 <body>
--- js/script.js
+++ js/script.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
 
 //it puts the lotion on the query
 
-
-
+$('.box').each(function(){
+	var color = this.dataset.color;
+	$(this).append('<div class="color"></div>')
+	$(this).children('.color').append(color);
+	$(this).attr('title',color);
+	$(this).css('background',color);
+});
 
 });
