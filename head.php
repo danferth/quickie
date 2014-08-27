7c82afecadbd4935fb394f0285af8c746e720db0
danferth <danferth@gmail.com>
2014-08-27 09:15:09 -0700

    finished adding svg icons. added svg-icons.html referance page

--- head.php
+++ head.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>just a quickie</title>
-	<link rel="stylesheet" type="text/css" href="css/font-awesome/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/svg.css"> 
 	<link rel="stylesheet" type="text/css" href="css/css.css"> 
 </head>
 <body>
